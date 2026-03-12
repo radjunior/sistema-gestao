c37b3b095b702ddb1b60e3c59a5ea1c011b64521
radjunior <reg.juninho@hotmail.com>
2026-03-12 00:56:39 -0300

    Update ErroController.java

--- src/main/java/br/com/gestao/controller/ErroController.java
+++ src/main/java/br/com/gestao/controller/ErroController.java
@@ -10,21 +10,21 @@ import jakarta.servlet.http.HttpServletRequest;
 @Controller
 public class ErroController implements ErrorController {
 
-    @RequestMapping("/error")
-    public String handleError(HttpServletRequest request) {
-        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-        if (status != null) {
-            int statusCode = Integer.parseInt(status.toString());
-            if (statusCode == 404) {
-                return "error/404";
-            }
-            if (statusCode == 403) {
-                return "error/403";
-            }
-            if (statusCode == 500) {
-                return "error/500";
-            }
-        }
-        return "error/generic";
-    }
+	@RequestMapping("/error")
+	public String handleError(HttpServletRequest request) {
+		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+		if (status != null) {
+			int statusCode = Integer.parseInt(status.toString());
+			if (statusCode == 404) {
+				return "error/404";
+			}
+			if (statusCode == 403) {
+				return "error/403";
+			}
+			if (statusCode == 500) {
+				return "error/500";
+			}
+		}
+		return "error/generic";
+	}
 }
