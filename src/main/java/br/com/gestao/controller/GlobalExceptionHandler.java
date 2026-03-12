4a4a016d50e76fe0dd6d9d264d2fcd4ff641f431
radjunior <reg.juninho@hotmail.com>
2026-03-12 00:49:00 -0300

    Update GlobalExceptionHandler.java

--- src/main/java/br/com/gestao/controller/GlobalExceptionHandler.java
+++ src/main/java/br/com/gestao/controller/GlobalExceptionHandler.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
-    public String handleException(Exception ex) {
-        return "error/500";
-    }
+	@ExceptionHandler(Exception.class)
+	public String handleException(Exception ex) {
+		return "error/500";
+	}
 
 }
